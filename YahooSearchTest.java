3e4f4eae29662e36f756182a269b6fca7cc02719
SQAPractical <sqapractical@gmail.com>
2018-10-06 14:44:45 -0700

    Adding YahooSearchTest

---
+++ YahooSearchTest.java
@@ -0,0 +1,20 @@
+package YahooSearch;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class YahooSearchTest {
+    private WebDriver driver;
+
+    @Test
+    public void testSearch() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "/Users/sofia/IdeaProjects/WebdriverJavaBootcamp/src/test/resources/geckodriver");
+        driver = new FirefoxDriver();
+//        navigatingToMainPage();
+//        typeRequestInSearchInput();
+//        submitSearch();
+//        printOutAmountOfResult();
+    }
+}
